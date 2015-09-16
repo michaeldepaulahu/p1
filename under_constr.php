2bd57f1b595ab5b6519f220a329fb1cdd9c3346d
“Michael <michaeldepaula@fas.harvard.edu>
2015-09-16 09:32:03 -0400

    Add under construction image and page

---
+++ under_constr.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>PAGE NOT FOUND</title>
+<meta charset="utf-8">    
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="michaeldepaula">
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css" >
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <div class="container-fluid">    
+        <!--header-->
+        <header>
+            <div class="row thumb1">
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <div class="title">
+                        MICHAEL <span class="csplit">DEPAULA</span>
+                        <p>Dynamic Web Apps Portfolio</p>
+                    </div>
+                </div>
+            </div> 
+        </header>    
+        <!--end header-->    
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 main_uc"></div>
+        </div> 
+        <div class="row thumb2">
+            <div class="col-md-12">
+            	<h1>Sorry! Page Not Available</h1>
+                <p>The page you are looking for is currently under construction.</p>
+                <p>Please check back later. Thank you.</p> 
+            </div> 
+        </div>   
+    <!--footer-->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <p class="f_title">Michael <span class="csplit">DEPAULA</span></p>
+                </div>
+                <div class="col-md-4">
+                    <p class="f_title">GitHub</p>
+                    <ul>
+                        <li><a href="https://github.com/michaeldepaulahu/p1">Home</a></li>
+                        <li><a href="https://github.com/michaeldepaulahu/p2">Password Generator</a></li>
+                        <li><a href="https://github.com/michaeldepaulahu/p3">Developer's Best Friend</a></li>
+                        <li><a href="https://github.com/michaeldepaulahu/p4">Web App</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <p class="f_title">Web Links</p>
+                    <ul>
+                        <li><a href="http://p1.dehashed.com/">Home</a></li>
+                        <li><a href="http://p2.dehashed.com/">Password Generator</a></li>
+                        <li><a href="http://p3.dehashed.com/">Developer's Best Friend</a></li>
+                        <li><a href="http://p4.dehashed.com/">Web App</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>   
+    </footer>
+   	<!--end footer--> 
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
