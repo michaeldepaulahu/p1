4476b6abc53764180bf86d7e06aa222cfc90a9d5
“Michael <michaeldepaula@fas.harvard.edu>
2015-09-16 12:42:57 -0400

    change js to add ul reference

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@
  	function toggle(sel){
 		// selectors
 		var p = sel + " p:eq(1)";
-		var p1 = sel + " ul";
+		var ul = sel + " ul";
 		var span = sel + " span:eq(0)";
 		var fig = sel + " figure";
 		var thb = sel + " .thumbnail";		
@@ -16,13 +16,13 @@
 		// hover function
 		$(sel).hover(function(){
 			$(p).fadeIn(500);
-			$(p1).fadeIn(500);
+			$(ul).fadeIn(500);
 			$(fig).css('opacity', 0.9); 
 			$(thb).css('background-color', '#e2edf0');
 			$(span).animate({'margin-top': '10px'}).clearQueue().finish();
 		}, function(){ 
 			$(p).fadeOut(500); 
-			$(p1).fadeOut(500); 
+			$(ul).fadeOut(500); 
 			$(fig).css('opacity', 1);
 			$(thb).css('background-color', '#ffffff');
 			$(span).animate({'margin-top': '10px'}).clearQueue().finish();
